218a01476e903b70aa7fb9356d69088c1121e823
iambrozjak <ivanochkavasyljuk@gmail.com>
2024-01-05 23:40:43 +0200

    Add change

--- game1.rb
+++ game1.rb
@@ -1,4 +1,8 @@
+require 'telegram/bot'
+
 class Game
+  attr_reader :board, :current_player
+
   def initialize(bot, message)
     @board = Array.new(3) { Array.new(3, '-') }
     @message = message
@@ -13,9 +17,54 @@ class Game
   end
 
   def make_move(x, y)
-    @board[x][y] = @current_player
-    switch_player
-    print_board
+    if valid_move?(x, y)
+      @board[x][y] = @current_player
+      print_board
+      switch_player
+      computer_move unless game_over?
+    else
+      send_message("Invalid move. Please try again.")
+    end
+  end
+
+  def computer_move
+    empty_cells = find_empty_cells
+
+    if empty_cells.any?
+      move = empty_cells.sample
+      @board[move[0]][move[1]] = @current_player
+      print_board
+      switch_player
+    end
+  end
+
+  def game_over?
+    winner = check_winner
+    return true if [:player, :opponent].include?(winner)
+    return :draw if winner == :draw
+
+    false
+  end
+
+  private
+
+  def print_board
+    markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(
+      inline_keyboard: @board.map.with_index do |row, i|
+        row.map.with_index do |cell, j|
+          Telegram::Bot::Types::InlineKeyboardButton.new(text: cell, callback_data: "#{i},#{j}")
+        end
+      end
+    )
+    send_message("Player's #{@current_player} turn", markup)
+  end
+
+  def switch_player
+    @current_player = (@current_player == @player) ? @opponent : @player
+  end
+
+  def valid_move?(x, y)
+    (0..2).cover?(x) && (0..2).cover?(y) && @board[x][y] == '-'
   end
 
   def find_empty_cells
@@ -30,19 +79,7 @@ class Game
     empty_cells
   end
 
-  def computer_move
-    empty_cells = find_empty_cells
-
-    if empty_cells.any?
-      move = empty_cells.first
-      make_move(move[0], move[1])
-      print_board
-    end
-  end
-
-
-
-  def check_winner(board)
+  def check_winner
     winning_combinations = [
       [0, 1, 2], [3, 4, 5], [6, 7, 8], # Rows
       [0, 3, 6], [1, 4, 7], [2, 5, 8], # Columns
@@ -50,36 +87,16 @@ class Game
     ]
 
     winning_combinations.each do |combo|
-      return :player if combo.all? { |i| board.flatten[i] == @player }
-      return :opponent if combo.all? { |i| board.flatten[i] == @opponent }
+      return :player if combo.all? { |i| @board.flatten[i] == @player }
+      return :opponent if combo.all? { |i| @board.flatten[i] == @opponent }
     end
 
-    return :draw unless board.flatten.include?('-')
+    return :draw unless @board.flatten.include?('-')
 
     nil
   end
 
-
-  def game_over?
-    winner = check_winner(@board)
-    return true if [:player, :opponent].include?(winner)
-    return :draw if winner == :draw
-
-    false
-  end
-
-  private
-
-  def print_board
-    markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: @board.map.with_index do |row, i|
-      row.map.with_index do |cell, j|
-        Telegram::Bot::Types::InlineKeyboardButton.new(text: cell, callback_data: "#{i},#{j}")
-      end
-    end)
-    @bot.api.send_message(chat_id: @message.chat.id, text: "Player's #{@current_player} turn", reply_markup: markup)
-  end
-
-  def switch_player
-    @current_player = @current_player == @player ? @opponent : @player
+  def send_message(text, markup = nil)
+    @bot.api.send_message(chat_id: @message.chat.id, text: text, reply_markup: markup)
   end
 end
